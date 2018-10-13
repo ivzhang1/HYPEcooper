b643e570e2bcb02116a1365f5179b0112d240e99
ivzhang1 <izhang1@stuy.edu>
2018-10-12 20:48:43 -0400

    added stuf

--- appy.py
+++ appy.py
@@ -3,18 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def form_example():
-    #print(app);
-    return render_template("form_example.html")
+def home():
 
-@app.route("/auth")
-def auth():
-    #print(request)
-    #print(request.headers)
-    #print(request.method)
-    #print(request.args['username'])
-    #print(request.form)
-    return render_template("greet.html", user = request.args['username'], method = request.method)
+    return render_template("home.html")
 
 if __name__ == "__main__":
 	app.debug = True
